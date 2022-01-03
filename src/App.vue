8b0005e2a809966b55dd934c9f5c61758bf8a063
Gabriel <gabriel52.alves@gmail.com>
2022-01-03 18:05:24 -0300

    new update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <cabecalho :valor="titulo" />
+    <div class="container">
+      <adicionar-tarefa @adicionarTarefa="adicionarTarefa" />
+      <listar-tarefas :tarefas="tarefas" />
+      <footer>
+        <em>Altere aqui o título da sua lista de tarefas</em>
+        <entrada-dados v-model="titulo" type="text" />
+      </footer>
+    </div>
+    <rodape />
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Cabecalho from "./components/Cabecalho.vue";
+import Rodape from "./components/Rodape.vue";
+import ListarTarefas from "./components/ListaTarefas.vue";
+import AdicionarTarefa from "./components/AdicionarTarefa.vue";
+import EntradaDados from "./components/templates/EntradaDados.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Cabecalho,
+    Rodape,
+    ListarTarefas,
+    AdicionarTarefa,
+    EntradaDados,
+  },
+  data() {
+    return {
+      titulo: "Minha lista de tarefas",
+      tarefas: [
+        { titulo: "Estudar", checked: false },
+        { titulo: "Programar", checked: true },
+      ],
+    };
+  },
+  methods: {
+    adicionarTarefa(novaTarefa) {
+      this.tarefas.push({
+        titulo: novaTarefa,
+        checked: false,
+      });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 60%;
+  margin: 20px auto 0px auto;
+}
+
+ul li {
+  list-style: none;
+}
+
+.removed {
+  color: gray;
+}
+
+.removed label {
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/AdicionarTarefa.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="input-group">
+    <entrada-dados @salvar="adicionarTarefa" v-model="novaTarefa" />
+    <span class="input-group-btn">
+      <button @click="adicionarTarefa" class="btn btn-primary">
+        Adicionar tarefa
+      </button>
+    </span>
+  </div>
+</template>
+
+<script>
+import EntradaDados from "./templates/EntradaDados.vue";
+export default {
+  components: { EntradaDados },
+  data() {
+    return {
+      novaTarefa: "",
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      this.$emit("adicionarTarefa", this.novaTarefa);
+      this.novaTarefa = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
